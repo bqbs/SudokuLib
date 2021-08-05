4195169a575b612e0e024222d9190abe1dce1142
lianjc <lianjinchu@@gznewsoft.com>
2021-08-05 14:43:24 +0800

    backup

---
+++ libsudoku/src/main/java/com/jclian/libsudoku/utils/Log.kt
@@ -0,0 +1,10 @@
+package com.jclian.libsudoku.utils
+
+/**
+ * created by lianjc on 2021/8/5
+ */
+object Log {
+    fun d(tag: String, message: String) {
+        println("$tag---------$message")
+    }
+}
